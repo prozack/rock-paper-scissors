04577e74b3f2f9a3de0c238ef3111676cb0f27a4
Zack Fullerton <zachary.e.fullerton@gmail.com>
2016-07-20 22:04:52 -0500

    First attempt at rps logic

--- rps.js
+++ rps.js
@@ -1,1 +1,17 @@
-rps.js
+
+var choice;
+var opponent_choice ;
+
+var opponent = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+opponent(1,3)
+
+if (opponent === 1) {
+	opponent_choice = 'rock';
+} else if (opponent === 2) {
+	opponent_choice = 'paper';
+} else {
+	opponent_choice = 'scissors';
+}
