b77ef4d20e1139d8fed2759f0852108a42d7a0c8
Zack Fullerton <zachary.e.fullerton@gmail.com>
2016-07-23 19:14:42 -0500

    Completed rps logic, tied into button selection in html

--- rps.js
+++ rps.js
@@ -2,33 +2,47 @@
 var choice;
 var opponent_choice;
 
+var input_choice = function(selection){
+	choice = selection;
+}
+
 //randomized bot choice
-var opponent_selection = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+var opponent = function() {
+    var opponent_selection = function(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    //calling function for a random number between 1 and 3
+    var opponent_random = opponent_selection(1,3)
+
+    //matching random choice to selection
+    if (opponent_random === 1) {
+	    opponent_choice = 'rock';
+    } else if (opponent_random === 2) {
+	    opponent_choice = 'paper';
+    } else {
+	    opponent_choice = 'scissors';
+    }
 }
-
-//calling function for a random number between 1 and 3
-opponent_selection(1,3)
-
-//matching random choice to selection
-if (opponent === 1) {
-	opponent_choice = 'rock';
-} else if (opponent === 2) {
-	opponent_choice = 'paper';
-} else {
-	opponent_choice = 'scissors';
-}
-
+//setting defaults to 0
 var wins = 0;
 var losses = 0;
 var draws = 0;
 
-if (choice === 'rock' && opponent_choice === 'scissors' || choice === 'scissors' && opponent_choice === 'paper' || choice === 'paper' && opponent_choice === 'paper'){
-	wins++;
-} else if (choice === 'rock' && opponent_choice === 'paper' || choice === 'scissors' && opponent_choice === 'rock' || choice === 'paper' && opponent_choice === 'scissors'){
-    losses++;
-} else {
-	draws++;
+//determining winner based on choices
+var game_outcome = function(){
+	opponent();
+	console.log("Your choice: ", choice, "Opponent's choice: ", opponent_choice);
+  if (choice === 'rock' && opponent_choice === 'scissors' || choice === 'scissors' && opponent_choice === 'paper' || choice === 'paper' && opponent_choice === 'paper'){
+	  wins++;
+  } else if (choice === 'rock' && opponent_choice === 'paper' || choice === 'scissors' && opponent_choice === 'rock' || choice === 'paper' && opponent_choice === 'scissors'){
+      losses++;
+  } else {
+	  draws++;
+  }
+  choice = undefined;
+  opponent_choice = undefined;
+  console.log(wins, losses, draws)
 }
 
 var startTimer = function() {
