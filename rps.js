49189b439009227460ebc046375315bc70569c88
Zack Fullerton <zachary.e.fullerton@gmail.com>
2016-07-24 21:00:36 -0500

    Finished timer logic, formatting on rps file, implemented reset game logic

--- rps.js
+++ rps.js
@@ -1,45 +1,51 @@
 
+//rock paper scissors game logic begins
 var choice;
 var opponent_choice;
 
-var input_choice = function(selection){
+//player's selection
+var inputChoice = function(selection){
 	choice = selection;
 }
 
 //randomized bot choice
 var opponent = function() {
-    var opponent_selection = function(min, max) {
+    var opponentSelection = function(min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     //calling function for a random number between 1 and 3
-    var opponent_random = opponent_selection(1,3)
+    var opponentRandom = opponentSelection(1,3)
 
     //matching random choice to selection
-    if (opponent_random === 1) {
-	    opponent_choice = 'rock';
-    } else if (opponent_random === 2) {
-	    opponent_choice = 'paper';
+    if (opponentRandom === 1) {
+	    opponentChoice = 'rock';
+    } else if (opponentRandom === 2) {
+	    opponentChoice = 'paper';
     } else {
-	    opponent_choice = 'scissors';
+	    opponentChoice = 'scissors';
     }
 }
+
 //setting default scores to 0
 var wins = 0;
 var losses = 0;
 var draws = 0;
+
 //counter to ensure all results are tallied
 var counter;
 
 //determining winner based on choices
-var game_outcome = function(){
+var gameOutcome = function(){
 	opponent();
-	console.log("Your choice: ", choice, "Opponent's choice: ", opponent_choice);
-  if (choice === 'rock' && opponent_choice === 'scissors' || choice === 'scissors' && opponent_choice === 'paper' || choice === 'paper' && opponent_choice === 'rock'){
+    document.getElementById("yourChoice").innerHTML = "You chose: " + choice;
+    document.getElementById("botChoice").innerHTML = "Opponent chose: " + opponentChoice;
+	console.log("Your choice: ", choice, "Opponent's choice: ", opponentChoice);
+  if (choice === 'rock' && opponentChoice === 'scissors' || choice === 'scissors' && opponentChoice === 'paper' || choice === 'paper' && opponentChoice === 'rock'){
 	  wins++;
 	  //write to HTML with updated score
 	  document.getElementById("htmlWins").innerHTML = "Wins: " + wins;
-  } else if (choice === 'rock' && opponent_choice === 'paper' || choice === 'scissors' && opponent_choice === 'rock' || choice === 'paper' && opponent_choice === 'scissors'){
+  } else if (choice === 'rock' && opponentChoice === 'paper' || choice === 'scissors' && opponentChoice === 'rock' || choice === 'paper' && opponentChoice === 'scissors'){
       losses++;
       document.getElementById("htmlLosses").innerHTML = "Losses: " + losses;
   } else {
@@ -48,25 +54,27 @@ var game_outcome = function(){
   }
   //resetting variables after outcome determined
   choice = undefined;
-  opponent_choice = undefined;
+  opponentChoice = undefined;
   counter = wins + losses + draws;
   console.log("wins: ", wins, "losses: ", losses, "draws: ", draws, "counter: ", counter);
 }
 
 //logic to determine winner, to be called when timer reaches zero
-var end_of_game = function() {
+var endOfGame = function() {
 	if (wins > losses) {
 		return "You win!";
 	} else if (wins < losses) {
-		return "You lose!"
+		return "You lose!";
 	} else {
-		return "It's a draw!"
+		return "It's a draw!";
 	}
 }
 
-var startTimer = function() {
-	var userMins = document.getElementById('mins').value;
-	var userSecs = document.getElementById('secs').value;
-	var userTime = (userMins * 60) + userSecs;
-	console.log(userTime)
+var clearValues = function(){
+	wins = 0;
+	losses = 0;
+	draws = 0;
+	document.getElementById("htmlWins").innerHTML = "Wins: " + wins;
+	document.getElementById("htmlLosses").innerHTML = "Losses: " + losses;
+	document.getElementById("htmlDraws").innerHTML = "Draws: " + draws;
 }
---
+++ timer.js
@@ -0,0 +1,68 @@
+//timer 
+var userMins = 0;
+var userSecs = 0;
+var startCount = 0;       // used to control when to read data from form
+
+function startTimer() {
+  // if $startchr is 0, and form fields exists, gets data for minutes and seconds, and sets $startchr to 1
+  if (startCount == 0 && document.getElementById('mins') && document.getElementById('secs')) {
+    // makes sure the script uses integer numbers
+    userMins = parseInt(document.getElementById('mins').value);
+    userSecs = parseInt(document.getElementById('secs').value);
+
+    // ensures value from form is a number
+    if (isNaN(userMins)) userMins = 0;
+    if (isNaN(userSecs)) userSecs = 0;
+
+    // rewrite data in form fields to be sure that the fields for minutes and seconds contain integer number
+    document.getElementById('mins').value = userMins;
+    document.getElementById('secs').value = userSecs;
+    startCount = 1;
+    document.getElementById('startButton').setAttribute('disabled', 'disabled');     // disable the button
+  }
+
+  // if minutes and seconds are 0, enables start button
+  if (userMins === 0 && userSecs === 0) {
+    startCount = 0;
+    document.getElementById('startButton').removeAttribute('disabled');     
+    //runs end of game calculation when timer reaches 0
+    alert(endOfGame());
+    //return false to prevent timer reset
+    return false;
+  }
+
+  //alert at 10 seconds 
+  else if (userMins === 0 && userSecs === 10) {
+    console.log("10 seconds left!");
+    //decrease seconds to continue countdown
+    userSecs--;
+  } else {
+    // decrease seconds, and decrease minutes if seconds reach to 0
+    userSecs--;
+    if (userSecs < 0) {
+      if (userMins > 0) {
+        userSecs = 59;
+        userMins--;
+      } else {
+        userSecs = 0;
+        userMins = 0;
+      }
+    }
+  }
+  
+  //formats minutes and seconds for countdown timer
+  var padDecimal = function(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+      return "0" + valString;
+    } else {
+      return valString;
+    }
+  }
+
+  // display the time in page, and auto-calls this function after 1 seccond
+  document.getElementById('displayMins').innerHTML = padDecimal(userMins);
+  document.getElementById('displaySecs').innerHTML = padDecimal(userSecs);
+  setTimeout('startTimer()', 1000);
+}
+
